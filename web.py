ebed2658a35b76ce6d49bcb6b3cf8c766bfd25d1
Marcos Martinez Valdez <105815107+Marcosmv124@users.noreply.github.com>
2024-09-10 16:31:36 +0000

    Guardar cambios

--- web.py
+++ web.py
@@ -1,26 +1,23 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import parse_qsl, urlparse
+import os
 
 
 class WebRequestHandler(BaseHTTPRequestHandler):
-    def url(self):
+     def url(self):
         return urlparse(self.path)
 
     def query_data(self):
         return dict(parse_qsl(self.url().query))
 
-   def do_GET(self):
-      # self.send_response(200)
-       #self.send_header("Content-Type", "text/html")
-       #self.end_headers()
-       #self.wfile.write(self.get_response().encode("utf-8"))
-          # Determinar si se solicita la página de inicio o una ruta no definida
+    def do_GET(self):
+        # Determinar si se solicita la página de inicio o una ruta no definida
         if self.url().path == '/':
             self.serve_home()
         else:
             self.serve_404()
 
-      def serve_home(self):
+    def serve_home(self):
         # Leer el contenido del archivo home.html
         try:
             with open('home.html', 'r') as file:
@@ -31,8 +28,8 @@ class WebRequestHandler(BaseHTTPRequestHandler):
             self.wfile.write(contenido.encode('utf-8'))
         except FileNotFoundError:
             self.serve_404()
-        
-      def serve_404(self):
+
+    def serve_404(self):
         # Leer el contenido del archivo 404.html
         try:
             with open('404.html', 'r') as file:
@@ -49,24 +46,25 @@ class WebRequestHandler(BaseHTTPRequestHandler):
             self.wfile.write(b"404 No Encontrado")
 
     def get_response(self):
-    # Extraer el path y la query string
-      path = self.url().path.strip('/')
+        # Extraer el path y la query string
+        path = self.url().path.strip('/')
         query_params = self.query_data()
-        #DEBUG: Imprimir los parámetros para revisar si se están capturando correctamente
+
+        # DEBUG: Imprimir los parámetros para revisar si se están capturando correctamente
         print(f"Path: {path}")
         print(f"Query Params: {query_params}")
 
         # Obtener el valor del parámetro 'autor', si existe
-       autor = query_params.get('autor', 'Desconocido')
+        autor = query_params.get('autor', 'Desconocido')
 
-        #Crear la respuesta HTML dinámica
-       return f"""
+        # Crear la respuesta HTML dinámica
+        return f"""
     <h1> Hola Web </h1>
     <p> URL Parse Result : {self.url()}         </p> 
     <p> Path Original: {self.path}         </p>
     <p> Headers: {self.headers}      </p>
     <p> Query: {self.query_data()}   </p>
-"""
+    """
 
 if __name__ == "__main__":
     print("Starting server")
